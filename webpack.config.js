f0f08e5229ba1015268c9f4a1ba0623d284b6df9
Pau Egea <papau84@gmail.com>
2022-04-10 22:07:36 +0200

    create templates characters

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
 const path = require('path');
-const HtmlwebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.export = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    },
-    resolve: {
-        extensions: ['.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlwebpackPlugion({
-            inject: true,
-            template: './public/index.html',
-            filename: './index.html'
-        })
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin([
+      {
+        inject: true,
+        template: './public/index.html',
+        filename: './index.html',
+      }
+    ]),
+  ]
 }
